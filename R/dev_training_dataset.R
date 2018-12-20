31e59d8e6c55b81074917009c1a1ca959b9ff93d
Aaron Chafetz <achafetz@usaid.gov>
2018-12-20 11:47:10 -0500

    rename Scripts folder

---
+++ R/dev_training_dataset.R
@@ -0,0 +1,98 @@
+##   Training Dataset
+##   Aaron Chafetz
+##   Purpose: create a training dataset for public/non-identifiable use
+##   Date: 2018-03-13
+##   Updated: 2018-07-27
+
+# NOTES:
+#  - user must input folderpath where files are stored locally
+#  - requires having Fact View stored locally, assumed different location than project folder
+#  - got_map.csv should not be posted publically
+
+
+
+gen_training <- function(msd_folderpath){
+  
+  #import new geography data
+    got_geo <- readr::read_csv("Input/got_geo.csv",
+                        col_types = readr::cols(.default = "c")) 
+  
+  #import fact view
+    df_mer <- readr::read_rds(Sys.glob(file.path(msd_folderpath, "MER_Structured_Dataset_PSNU_IM_FY17*.rds")))
+  
+  #randomly select a ~25 psnus
+    #identify current period and prior fy apr
+    curr_pd <- ICPIutilities::identifypd(df_mer)
+    prior_apr <- paste0("fy",ICPIutilities::identifypd(df_mer, "year") - 1,"apr")
+    
+    #no dedups or mil, must have either FY17 or FY18 data, drop any line with missing values
+    cohort <- df_mer %>% 
+      dplyr::filter(typemilitary == "N", mechanismid > 1) %>% 
+      dplyr::select(region:implementingmechanismname, prior_apr, curr_pd) %>% 
+      dplyr::mutate_at(dplyr::vars(prior_apr, curr_pd), ~ ifelse(. == 0, NA, .)) %>% 
+      tidyr::drop_na(region:psnuuid, mechanismid:implementingmechanismname, prior_apr, curr_pd) %>% 
+      dplyr::select(-prior_apr, -curr_pd) %>% 
+      dplyr::distinct() %>% 
+      dplyr::sample_n(nrow(got_geo))
+    
+      rm(curr_pd, prior_apr)
+  #bind the masked geography to the real
+    got_map <- dplyr::bind_cols(got_geo, cohort)
+      rm(cohort, got_geo)
+    #export for documentation 
+      fs::dir_create("Input")
+      readr::write_csv(got_map, "Input/got_map.csv")
+  
+  #bind the masked data on, remove the original geo data and rename the variables  
+    got <- dplyr::left_join(got_map, df_mer) %>%
+      dplyr::select(-region:-psnuuid) %>% 
+      dplyr::rename_all(~ stringr::str_remove(., "got_"))
+      rm(got_map)
+  #remove the mech UID
+    got <- got %>% 
+      dplyr::mutate(mechanismuid  = "")
+    
+  #identify the number of mechanisms create masked ones and bind back on, replacing the originals
+    mechs <- dplyr::distinct(got, mechanismid) %>% 
+             dplyr::mutate(mechanismid_new = dplyr::row_number() + 80000,
+                           mechanismid_new = as.character(mechanismid_new))
+    got <- dplyr::full_join(got, mechs)
+      rm(mechs)
+    got <- got %>% 
+      dplyr::mutate(mechanismid = mechanismid_new) %>% 
+      dplyr::select(-mechanismid_new)
+  
+  #identify the number of mechanisms create masked ones and bind back on, replacing the originals
+    primepartner <- dplyr::distinct(got, primepartner)
+    got_pp <- readr::read_csv("Input/got_primepartner.csv",
+                              col_types = readr::cols(.default = "c"))
+    got_pp <- got_pp %>% 
+      dplyr::sample_n(nrow(primepartner))
+    primepartner <- dplyr::bind_cols(primepartner, got_pp)
+      rm(got_pp)
+    got <- dplyr::full_join(got, primepartner)
+      rm(primepartner)
+    got <- got %>% 
+      dplyr::mutate(primepartner = got_primepartner,
+                    implementingmechanismname = got_primepartner) %>% 
+      dplyr::select(-got_primepartner)
+  
+  #adjust values, 60% of value & rounding to the nearest 10
+    got <- got %>% dplyr::mutate_if(is.numeric, ~ 10*ceiling((.*0.60)/10))
+  
+  #remove zeros
+    got <- got %>% dplyr::mutate_if(is.numeric, ~ ifelse(. == 0, NA, .))
+    
+  #convert back to uppercase names
+    headr <- readr::read_tsv(Sys.glob(file.path(msd_folderpath, "MER_Structured_Dataset_PSNU_IM_FY17*.txt")), 
+                      col_types = readr::cols(.default = "c"),
+                      n_max = 0) %>%
+      names()
+    
+    names(got) = headr
+      rm(headr)
+    
+  #export
+    readr::write_tsv(got, "Output/MER_Structured_TRAINING_Dataset_PSNU_IM_FY17-18_20181115_v1_1.txt", na = "")
+    rm(got, df_mer)                          
+}
---
+++ R/planets_training_dataset.R
@@ -0,0 +1,120 @@
+##   Training Dataset
+##   Aaron Chafetz
+##   Purpose: create a training dataset for public/non-identifiable use
+##   Date: 2018-12-19
+##   Updated: 
+
+
+# NOTES -------------------------------------------------------------------
+
+#  - user must supply filepath for local PSNUxIM MSD
+#  - requires having MSD (.txt) stored locally
+#  - requires having planets_psnuuids2map.csv saved in Input which identifies the actual PSNUs that will be masked
+
+
+# DEPENDENCIES ------------------------------------------------------------
+
+library(magrittr)
+
+
+# FUNCTION ----------------------------------------------------------------
+
+
+gen_training_planets <- function(msd_filepath){
+  
+  #set seed for sampling to ensure same order ever time
+    set.seed(14)
+  
+  #import new geography data
+    planets_geo <- readr::read_csv("Input/planets_geo.csv",
+                             col_types = readr::cols(.default = "c")) %>% 
+      dplyr::rename_all(tolower)
+  
+  #bind list of real PSNUs onto new geography data frame
+    planets_geo <- readr::read_csv("Input/planets_psnuuids2map.csv",
+                    col_types = readr::cols(.default = "c")) %>% 
+      dplyr::bind_cols(planets_geo)
+    
+  #import MSD
+    df_mer <- ICPIutilities::read_msd(msd_filepath, to_lower = FALSE)
+
+  #store upper & lower case headers for applying ordering and snake casing
+    headr <- names(df_mer)
+    order <- tolower(headr)
+    
+  #convert to lower for ease of use
+    df_mer <- dplyr::rename_all(df_mer, tolower)
+    
+  #keep data only from select PSNUs, bind the masked geography to the real
+    df_mw <- dplyr::inner_join(df_mer, planets_geo, by = "psnuuid")
+    
+    rm(df_mer)
+    
+  #remove the mech UID
+    df_mw <- df_mw %>% 
+      dplyr::mutate(mechanismuid  = "")
+  
+  #identify the number of mechanisms create masked ones and bind back on, replacing the originals
+    mechs <- df_mw %>% 
+      dplyr::distinct(mechanismid) %>% 
+      dplyr::arrange(mechanismid)
+  
+    new_mechs <- seq(1200, 1800,1) %>% #mechanism id can fall between 1200 and 1800
+      sample(nrow(mechs)) %>% #sample n number of mech ids based on n of mechanisms
+      as.character(.) %>% 
+      dplyr::bind_cols(mechs, mechanismid_mw = .) %>% 
+      dplyr::mutate(mechanismid_mw = ifelse(mechanismid %in% c("00000", "00001"), 
+                                            stringr::str_replace(mechanismid, "0000", "000"),
+                                            mechanismid_mw),
+                    mechanismid_mw = paste0("0", mechanismid_mw)) 
+  
+  #add masked partner & mechanism names
+    partners <- readr::read_csv("Input/planets_primepartners.csv",
+                                col_types = readr::cols(.default = "c")) %>% 
+      dplyr::rename(primepartner_mw = constellation,
+                    implementingmechanismname_mw = star_name) %>% 
+      dplyr::sample_n(nrow(mechs)) %>% #sample primeparter and mech names from list
+      dplyr::bind_cols(new_mechs, .) #bind onto mechanism list
+
+  #bind mechanism info onto dataset
+    df_mw <- df_mw %>% 
+      dplyr::select(mechanismid) %>% 
+      dplyr::left_join(., partners, by = "mechanismid") %>% 
+      dplyr::select(-mechanismid) %>% 
+      dplyr::bind_cols(df_mw, .) 
+  
+  #remove the original geo data and rename the variables  
+    unmasked <- df_mw %>% 
+      dplyr::select(dplyr::ends_with("_mw")) %>% 
+      names() %>% 
+      stringr::str_remove("_mw")
+    
+    df_mw <- df_mw %>% 
+      dplyr::select(-unmasked) %>% 
+      dplyr::rename_all(~ stringr::str_remove(., "_mw")) %>% 
+      dplyr::select(order)
+  
+  #adjust values, 60% of value & rounding to the nearest 10 and then remove zeros
+    df_mw <- df_mw %>% 
+      dplyr::mutate_if(is.numeric, ~ 10*ceiling((.*0.60)/10)) %>% 
+      dplyr::mutate_if(is.numeric, ~ ifelse(. == 0, NA, .))
+
+  #convert back to uppercase names
+    names(df_mw) <- headr
+  
+  #export
+    msd_filepath <- msd_filepath %>% 
+      basename() %>% 
+      stringr::str_replace("MER_Structured_Dataset", "MER_Structured_TRAINING_Dataset") 
+      
+    readr::write_tsv(df_mw, file.path("Output/", msd_filepath), na = "")
+} 
+
+
+# GENERATE TRAINING DATASET -----------------------------------------------
+
+#filepath for MSD (.txt)
+  msd_filepath <- "~/ICPI/Data/MER_Structured_Dataset_PSNU_IM_FY17-18_20181115_v1_2.txt"
+
+#genreate training dataset
+  gen_training_planets(msd_filepath)
