da4ea2d7f2e7c7ce80d4d55aeb9529b67d11dc13
Aaron Chafetz <achafetz@usaid.gov>
2018-12-21 13:01:08 -0500

    update data and data documentation

---
+++ R/planets_site_types.R
@@ -0,0 +1,7 @@
+#' List of site types
+#' 
+#' A vector of facility types to help mask site name.
+#' 
+#' @format A vector of 7
+#' 
+"planets_site_types"
---
+++ R/planets_sites.R
@@ -0,0 +1,9 @@
+#' List of astronomy terms for site names
+#' 
+#' A vector of astronomy terms to mask site names
+#' 
+#' @format A vector of 266 
+#' @source Glossary of astronomy, Wikipedia 
+#'   \url{https://en.wikipedia.org/wiki/Glossary_of_astronomy}
+#' 
+"planets_sites"
