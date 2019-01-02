4e010b98a52c2a4d8ee347904a3a341123a0bb8e
Aaron Chafetz <achafetz@usaid.gov>
2019-01-02 14:18:17 -0500

    work with stored data

--- R/mask_msd.R
+++ R/mask_msd.R
@@ -16,18 +16,20 @@
 
 mask_msd <- function(msd_filepath, ...){
   
+  #store PSNU UIDS
+    psnus <- list(...)
+  
+  #exit if 15 PSNU UIDS are not supplied
+    if(length(psnus) == 0)   stop("No PSNU UID list supplied")
+    #if(length(psnus) != 15)  stop("Must supply 15 PSNU UIDs.")
+  
   #set seed for sampling to ensure same order ever time
     set.seed(14)
-  
-  #import new geography data
-    planets_geo <- readr::read_csv("Input/planets_geo.csv",
-                             col_types = readr::cols(.default = "c")) %>% 
-      dplyr::rename_all(tolower)
-  
-  #bind list of real PSNUs onto new geography data frame
-    planets_geo <- readr::read_csv("Input/planets_psnuuids2map.csv",
-                    col_types = readr::cols(.default = "c")) %>% 
-      dplyr::bind_cols(planets_geo)
+    
+  #change geography data to lower & bind LIST of real PSNUs onto new/masked geography DATA FRAME
+    df_mapping <- planets_geo %>% 
+      dplyr::rename_all(tolower) %>% 
+      dplyr::bind_cols(psnuuid = psnus, .)
     
   #import MSD
     df_mer <- ICPIutilities::read_msd(msd_filepath, to_lower = FALSE)
@@ -40,13 +42,12 @@ mask_msd <- function(msd_filepath, ...){
     df_mer <- dplyr::rename_all(df_mer, tolower)
     
   #keep data only from select PSNUs, bind the masked geography to the real
-    df_mw <- dplyr::inner_join(df_mer, planets_geo, by = "psnuuid")
+    df_mw <- dplyr::inner_join(df_mer, df_mapping, by = "psnuuid")
     
     rm(df_mer)
     
   #remove the mech UID
-    df_mw <- df_mw %>% 
-      dplyr::mutate(mechanismuid  = "")
+    df_mw <- dplyr::mutate(df_mw, mechanismuid  = as.character(NA))
   
   #identify the number of mechanisms create masked ones and bind back on, replacing the originals
     mechs <- df_mw %>% 
@@ -56,33 +57,30 @@ mask_msd <- function(msd_filepath, ...){
     new_mechs <- seq(1200, 1800,1) %>% #mechanism id can fall between 1200 and 1800
       sample(nrow(mechs)) %>% #sample n number of mech ids based on n of mechanisms
       as.character(.) %>% 
-      dplyr::bind_cols(mechs, mechanismid_mw = .) %>% 
+      dplyr::bind_cols(mechs, mechanismid_mw = .) %>% #combine masked id with dedup
       dplyr::mutate(mechanismid_mw = ifelse(mechanismid %in% c("00000", "00001"), 
                                             stringr::str_replace(mechanismid, "0000", "000"),
                                             mechanismid_mw),
                     mechanismid_mw = paste0("0", mechanismid_mw)) 
   
   #add masked partner & mechanism names
-    partners <- readr::read_csv("Input/planets_primepartners.csv",
-                                col_types = readr::cols(.default = "c")) %>% 
+    partners <- planets_primepartners %>% 
       dplyr::sample_n(nrow(mechs)) %>% #sample primeparter and mech names from list
-      dplyr::bind_cols(new_mechs, .) #bind onto mechanism list
+      dplyr::bind_cols(new_mechs, .) %>% #bind onto mechanism list
+      dplyr::mutate(primepartner_mw = ifelse(mechanismid %in% c("00000", "00001"), "Dedup", primepartner_mw),
+                    implementingmechanismname_mw = ifelse(mechanismid %in% c("00000", "00001"), "Dedup", implementingmechanismname_mw))
 
   #bind mechanism info onto dataset
-    df_mw <- df_mw %>% 
-      dplyr::select(mechanismid) %>% 
-      dplyr::left_join(., partners, by = "mechanismid") %>% 
-      dplyr::select(-mechanismid) %>% 
-      dplyr::bind_cols(df_mw, .) 
+    df_mw <- dplyr::left_join(df_mw, partners, by = "mechanismid")
   
-  #remove the original geo data and rename the variables  
-    unmasked <- df_mw %>% 
+  #remove the original hiearchy/partner data and rename the variables and reorder 
+    lst_unmaskedvar <- df_mw %>% 
       dplyr::select(dplyr::ends_with("_mw")) %>% 
       names() %>% 
       stringr::str_remove("_mw")
     
     df_mw <- df_mw %>% 
-      dplyr::select(-unmasked) %>% 
+      dplyr::select(-lst_unmaskedvar) %>% 
       dplyr::rename_all(~ stringr::str_remove(., "_mw")) %>% 
       dplyr::select(order)
   
